f0c37ef32b9315049f15fb6858cbd4eb8f5e34e4
zb89426 <757571055@qq.com>
2014-10-25 20:06:13 +0800

    busappServer

---
+++ src/main/java/com/springapp/util/UrlFilter.java
@@ -0,0 +1,29 @@
+package com.springapp.util;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+/**
+ * Created by thanks_bin on 2014/10/25.
+ */
+public class UrlFilter implements Filter{
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+            HttpServletRequest request = (HttpServletRequest)servletRequest;
+            String context = request.getContextPath();
+            String uri = request.getRequestURI();
+            System.out.println(request.getParameterMap());
+            String path = uri.substring(context.length());
+            request.getRequestDispatcher(path).forward(servletRequest, servletResponse);
+        }
+
+    @Override
+    public void destroy() {
+
+    }
+}
